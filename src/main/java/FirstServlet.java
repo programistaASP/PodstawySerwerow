a6fc871e41aa671e6c9cb494a7e45095afe0532a
witar <witar1@gmail.com>
2019-12-01 10:31:45 +0100

    Initial commit

---
+++ src/main/java/FirstServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello from first servlet!");
+//        resp.sendRedirect("https://www.google.pl");
+
+        
+    }
+}
